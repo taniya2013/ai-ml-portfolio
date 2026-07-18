8a811fed223531042bcde63ea4217672506f5d95
Taniya <tt835554@gmail.com>
2026-07-18 11:23:37 +0530

    Delete check_model.py

--- check_model.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AQ.Ab8RN6Jt1Cfe0Lfy4o273GPb5Bn-_yzGIfkvc3Nt5j0FO7tTPQ")
-
-models = client.models.list()
-
-for model in models:
-    print(model.name)
